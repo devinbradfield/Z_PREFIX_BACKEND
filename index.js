b153e8e65ad9d1367e2003e94ae08737453bba0b
devinbradfield <devinbradfield@gmail.com>
2022-03-23 15:56:08 -0600

    s

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = require("./app");
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-    console.log(`Server is listening at localhost on port: ${port}`);
+    console.log(`Server is listening on port: ${port}`);
   });
   
